be5c6ae157253a47c8ed536c7206fe01a43bbd30
Ngô Văn Quân <25648131.quan@student.iuh.edu.vn>
2025-11-16 22:52:09 +0700

    change

--- main.js
+++
@@ -1,5 +0,0 @@
-let Food = ["Cơm tấm", "Xôi thịt", "Bún chả", "Bún riêu", "Bánh mì"];
-function click() {
-    let randomIdenx = Math.floor(Math.random() * Food.length);
-    document.getElementById("demo").innerHTML = Food[randomIdenx];
-}
